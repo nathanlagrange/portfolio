4e73582261f1535042ac011318872569cb18be26
nathanlagrange <62388778+nathanlagrange@users.noreply.github.com>
2021-12-10 23:27:15 +0100

    fix aos bug mobile

---
+++ portfolio-nathan/dist/_nuxt/static/1639175208/blog/21w24/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639175208",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F21w24",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3d1e0a30"}}}}(null));
--- portfolio-nathan/plugins/aos.js
+++ portfolio-nathan/plugins/aos.js
@@ -1,9 +1,9 @@
-import Aos from 'aos'
+import aos from 'aos'
 import 'aos/dist/aos.css'
 
 export default ({ app }) => {
-  app.Aos = new Aos.init({
-    disable: window.innerWidth < 640,
-    once: true
+  app.aos = new aos.init({
+    disable: false,
+    once: false
   })
 }
