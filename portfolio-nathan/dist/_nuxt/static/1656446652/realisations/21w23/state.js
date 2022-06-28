b05d186687b0258efd7aca17f0c532b8b426d4f6
nathanlagrange <nathanlagrange98@gmail.com>
2022-06-28 22:05:32 +0200

    hero light

---
+++ portfolio-nathan/dist/_nuxt/static/1656446652/realisations/21w23/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656446652",layout:"default",error:a,serverRendered:true,routePath:"\u002Frealisations\u002F21w23",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"317f10c6"}}}}(null));
